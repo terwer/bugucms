b65a93008ba6d32424b0dc3c3ec0820933202b5a
terwer <youweics@163.com>
2022-06-17 15:02:57 +0800

    fix:修复docker构建脚本

--- devrun.sh
+++ devrun.sh
@@ -2,6 +2,8 @@
 # 构建和运行
 ./mvnw clean package -DskipTests
 
+# 注意：生产环境这里需要清理工作目录
+
 cd target
 pwd
 mv ./classes/Dockerfile ./Dockerfile
@@ -9,4 +11,5 @@ mv ./classes/docker-compose.yml ./docker-compose.yml
 pwd
 ls -l
 
-docker compose up --build
+docker compose up -d --build
+docker ps
