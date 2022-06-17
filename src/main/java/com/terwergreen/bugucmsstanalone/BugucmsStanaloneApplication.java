0ea3db937b51442d9e323da6f982f1cf4e6ab0d5
terwer <youweics@163.com>
2022-06-17 14:23:40 +0800

    fix:解决代码扫描问题

--- src/main/java/com/terwergreen/bugucmsstanalone/BugucmsStanaloneApplication.java
+++ src/main/java/com/terwergreen/bugucmsstanalone/BugucmsStanaloneApplication.java
@@ -3,6 +3,9 @@ package com.terwergreen.bugucmsstanalone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author terwer
+ */
 @SpringBootApplication
 public class BugucmsStanaloneApplication {
 
