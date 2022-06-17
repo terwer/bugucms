318e602e161f42120aa0ed48381f910615fa5f7b
terwer <youweics@163.com>
2022-06-17 10:48:08 +0800

    feat:补充项目文件

---
+++ src/main/java/com/terwergreen/bugucmsstanalone/BugucmsStanaloneApplication.java
@@ -0,0 +1,13 @@
+package com.terwergreen.bugucmsstanalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugucmsStanaloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugucmsStanaloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/terwergreen/bugucmsstanalone/BugucmsStanaloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terwergreen.bugucmsstanalone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugucmsStanaloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
